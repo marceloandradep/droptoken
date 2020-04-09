b3e9cfc758bb06b369c4d35f242461e0cafdd90f
Marcelo Pereira <marceloandradep@gmail.com>
2020-04-09 16:49:35 -0700

    Implement board factory

--- src/main/java/com/_98point6/droptoken/model/factories/BoardFactory.java
+++ src/main/java/com/_98point6/droptoken/model/factories/BoardFactory.java
@@ -1,13 +1,28 @@
 package com._98point6.droptoken.model.factories;
 
+import com._98point6.droptoken.exceptions.InvalidBoardSizeException;
+import com._98point6.droptoken.model.board.DefaultBoard;
 import com._98point6.droptoken.model.interfaces.Board;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BoardFactory {
+
+    static final int MIN_BOARD = 4;
+    
+    private int maxBoard;
+
+    public BoardFactory(@Value("${game.max-board}") int maxBoard) {
+        this.maxBoard = maxBoard;
+    }
     
     public Board create(int size) {
-        return null;
+        if (size < MIN_BOARD || size > maxBoard) {
+            throw new InvalidBoardSizeException();
+        }
+        
+        return new DefaultBoard(size);
     }
     
 }
