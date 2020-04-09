14c24364cf3e30588783d2b8f6fbe80e7273cac1
Marcelo Pereira <marceloandradep@gmail.com>
2020-04-09 16:49:54 -0700

    Implement game factory

---
+++ src/main/java/com/_98point6/droptoken/model/factories/GameFactory.java
@@ -0,0 +1,34 @@
+package com._98point6.droptoken.model.factories;
+
+import com._98point6.droptoken.model.Game;
+import com._98point6.droptoken.model.interfaces.Board;
+import com._98point6.droptoken.model.interfaces.MoveList;
+import com._98point6.droptoken.model.interfaces.PlayerSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GameFactory {
+    
+    private PlayerSetFactory playerSetFactory;
+    private MoveListFactory moveListFactory;
+    private BoardFactory boardFactory;
+
+    @Autowired
+    public GameFactory(PlayerSetFactory playerSetFactory, MoveListFactory moveListFactory, BoardFactory boardFactory) {
+        this.playerSetFactory = playerSetFactory;
+        this.moveListFactory = moveListFactory;
+        this.boardFactory = boardFactory;
+    }
+
+    public Game createGame(List<String> players, int size) {
+        PlayerSet playerSet = playerSetFactory.create(players);
+        Board board = boardFactory.create(size);
+        MoveList moveList = moveListFactory.create();
+        
+        return new Game(playerSet, moveList, board);
+    }
+    
+}
